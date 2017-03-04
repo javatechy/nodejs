27e1d10f2f9e1f2838fd71fd6a703887e786b9b0
javatechy <deepak.kumar.iet@gmail.com>
2017-03-04 23:05:45 +0530

    removed node modules

---
+++ DirectoryWatcher/MultiDirWatch.js
@@ -0,0 +1,28 @@
+// Run mvn clean install on aggregator2 directory in case of any change
+//  install chokidar  :  npm install chokidar --save
+
+var fs = require("fs");
+var sys = require('sys')
+var chokidar = require('chokidar');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+watchDir("D:\\GitWork\\aggregator2");
+watchDir("D:\\GitWork\\Auto\\zauto");
+watchDir("D:\\GitWork\\query-service");
+watchDir("D:\\GitWork\\reporting");
+
+function watchDir(loc){
+	var watcher = chokidar.watch(loc+"\\src", {ignored: /^\./, persistent: true});
+	console.log("Strarting");
+	watcher
+		.on('add', function(path) {console.log('File', path, 'has been added');})
+		.on('change', function(path) {
+		console.log('File', path, 'has been changed');
+		exec("cd "+loc +" & mvn clean install", puts);
+		})
+	.on('unlink', function(path) {console.log('File', path, 'has been removed');})
+	.on('error', function(error) {console.error('Error happened', error);})
+	console.log("Finished");	
+};
+
--- README.md
+++ README.md
@@ -3,10 +3,21 @@
 
 This is simple node js operations project
 
-Projects:
+#Projects:
 DirectoryWatcher :  monitors a directory if any changes found in the mentioned directory it runs mvn clean install
 run this command  to install dependent module :
 npm install chokidar --save
+
+Create a shorcut on desktop to monitor:
+1. Create a text file
+2. Add the following content in it 
+	D:
+	cd JavaTechy\nodejs\DirectoryWatcher
+	node MultiDirWatch.js
+3. Save the file as .bat extension
+4. Run the bat file  to monitor
+
+
 
 Run a node js File
 node MvnChangeWatch.js
