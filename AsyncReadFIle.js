d1368cd3b57a6ee20107228ffae59e1d3669a5b5
deepak.kumar2 <deepak.kumar2@MCGL-5406.COMVIVA.COM>
2017-03-04 20:13:25 +0530

    First Commit

---
+++ AsyncReadFIle.js
@@ -0,0 +1,13 @@
+var fs=require('fs');
+
+var test= function(err,data){
+
+if(err) return  console.log("Error"+err);
+
+console.log("Program Ended"+data.toString());
+};
+
+
+fs.readFile('main_server.js',test());
+
+console.log("Program Ended");
---
+++ ReadFileSync.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+
+var x =  fs.readFileSync("sample.json");
+// Console will print the message
+var dataField=JSON.parse(x);
+console.log('Data=FIELD=========>',dataField);
+console.log('Data==========>'+JSON.stringify(dataField));
+console.log('OP CODE==========>'+dataField.operationCode);
+var x =  fs.readFileSync("writeIntoFileText");
